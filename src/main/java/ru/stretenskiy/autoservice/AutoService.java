ac7448cccd25a3da4c36159d54326435cab0558b
TheMadman <stretenskijdanila@gmail.com>
2023-12-17 00:51:23 +0300

    Rename Main->AutoService

--- src/main/java/ru/stretenskiy/autoservice/AutoService.java
+++ src/main/java/ru/stretenskiy/autoservice/AutoService.java
@@ -1,13 +1,10 @@
 package ru.stretenskiy.autoservice;
 
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.stretenskiy.autoservice.configs.MyConfig;
-import ru.stretenskiy.autoservice.entities.Cars;
 
 @SpringBootConfiguration
-public class Main {
+public class AutoService {
 
     public static void main(String[] args) {
 
