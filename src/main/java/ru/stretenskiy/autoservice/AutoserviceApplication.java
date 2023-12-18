a01e9b61a9188581acf75403a6f77456fbe01470
TheMadman <stretenskijdanila@gmail.com>
2023-12-19 00:14:48 +0300

    Обнуление всех работ. Начало с чистого листа

--- src/main/java/ru/stretenskiy/autoservice/AutoserviceApplication.java
+++ src/main/java/ru/stretenskiy/autoservice/AutoserviceApplication.java
@@ -1,32 +1,13 @@
 package ru.stretenskiy.autoservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.stretenskiy.autoservice.entity.Work;
-import ru.stretenskiy.autoservice.repository.CarsRepository;
-import ru.stretenskiy.autoservice.repository.WorksRepository;
 
-import java.util.List;
-
-@SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
-public class AutoserviceApplication implements CommandLineRunner {
+@SpringBootApplication
+public class AutoserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoserviceApplication.class, args);
 	}
 
-	@Autowired
-	WorksRepository worksRepository;
-
-	@Autowired
-	CarsRepository carsRepository;
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		//System.out.println(worksRepository.findAll());
-		//System.out.println(carsRepository.findAll());
-	}
 }
--- src/main/java/ru/stretenskiy/autoservice/repository/CarsRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.stretenskiy.autoservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.stretenskiy.autoservice.entity.Car;
-
-@Repository
-public interface CarsRepository extends JpaRepository<Car, Long> {
-}
